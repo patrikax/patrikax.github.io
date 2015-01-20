9f284f0f21a6cb7f161f412f6395e535e3d3f77a
Patrik Axelsson <patrik.axelsson@gmail.com>
2015-01-20 22:42:55 +0100

    updated flexslider and fixed slider issues for iOS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,37 @@
 // Initialise FlexSlider for Carousels
 $(window).load(function() {
+
   
   if(navigator.userAgent.match(/(iPhone|iPod|iPad)/i)) {
+
     $('.flexslider').flexslider({
+      touch: true,
+      slideshow: false,
+      useCSS: true,
+      maxItems: 1,
       animation: "slide",
-      animationSpeed: 100,
+      start: function(){
+        $('.flexslider').resize();
+      }
+    });
+  }
+  
+  else {
+    $('.flexslider').flexslider({
+      animation: "fade",
+      animationSpeed: 0,
       slideshow: false,
       touch: true,
       smoothHeight: false
     });
   }
-  else {
-  $('.flexslider').flexslider({
-    animation: "fade",
-    animationSpeed: 0,
-    slideshow: false,
-    touch: true,
-    smoothHeight: false
-  });
-  };
 
   $('.info-link').click(function(){
   	event.preventDefault();
     $(this).closest('.post').find('.info-box').toggle();
   });
 
-   $('.close-button').click(function(){
+  $('.close-button').click(function(){
     event.preventDefault();
     $(this).closest('.post').find('.info-box').hide();
   });
